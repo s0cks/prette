b0e3f43952c131da85528342c55784cca02fa139
Tazz <tazz@tazz.cloud>
2024-09-14 04:30:21 -0400

    ci(pre-commit): disable clang-format in pre-commit

--- Sources/prette/uri.h
+++ Sources/prette/uri.h
@@ -4,22 +4,22 @@
 #include <set>
 #include <cstdio>
 #include <string>
+#include <utility>
 #include <optional>
-#include <unordered_set>
 #include <fmt/format.h>
+#include <unordered_set>
 
-#include "prette/rx.h"
 #include "prette/common.h"
 
 namespace prt {
   namespace uri {
-    typedef std::unordered_set<std::string> ExtensionSet;
-    typedef std::string basic_uri;
-    typedef std::string Path;
-    typedef std::string Scheme;
-    typedef std::string Fragment;
-    typedef std::string Extension;
-    typedef std::unordered_map<std::string, std::string> QueryMap;
+    using ExtensionSet = std::unordered_set<std::string>;
+    using basic_uri = std::string;
+    using Path = std::string;
+    using Scheme = std::string;
+    using Fragment = std::string;
+    using Extension = std::string;
+    using QueryMap = std::unordered_map<std::string, std::string>;
 
     void SanitizePath(Path& value, const bool root = true);
     void SanitizeExtension(Extension& value);
@@ -32,14 +32,14 @@ namespace prt {
       Fragment fragment;
       QueryMap query;
     private:
-      Uri(const Scheme& s, const Uri& other):
-        scheme(s),
+      Uri(Scheme s, const Uri& other):
+        scheme(std::move(s)),
         path(other.path),
         fragment(other.fragment),
-        query(other.query) {  
+        query(other.query) {
       }
-      Uri(const Scheme& s, const Path& root, const Uri& path):
-        scheme(s),
+      Uri(Scheme s, const Path& root, const Uri& path):
+        scheme(std::move(s)),
         path(fmt::format("{0:s}/{1:s}", root, path.path[0] == '/' ? path.path.substr(1) : path.path)),
         fragment(path.fragment),
         query(path.query) {
@@ -47,210 +47,212 @@ namespace prt {
     public:
       Uri() = default;
       Uri(const basic_uri& uri);
-      Uri(const Scheme& s,
-          const Path& p,
-          const Fragment& f,
-          const QueryMap& q):
-        scheme(s),
-        path(p),
-        fragment(f),
-        query(q) {
+      Uri(Scheme s,
+          Path p,
+          Fragment f,
+          QueryMap q):
+        scheme(std::move(s)),
+        path(std::move(p)),
+        fragment(std::move(f)),
+        query(std::move(q)) {
       }
+      Uri(Uri&& rhs) = default;
       Uri(const Uri& rhs) = default;
       ~Uri() = default;
 
-      bool HasScheme() const;
-      bool HasScheme(const Scheme& a) const;
-      bool HasScheme(const Scheme& a,
-                     const Scheme& b) const;
-      bool HasScheme(const Scheme& a,
+      auto HasScheme() const -> bool;
+      auto HasScheme(const Scheme& a) const -> bool;
+      auto HasScheme(const Scheme& a,
+                     const Scheme& b) const -> bool;
+      auto HasScheme(const Scheme& a,
                      const Scheme& b,
-                     const Scheme& c) const;
-      bool HasScheme(const Scheme& a,
+                     const Scheme& c) const -> bool;
+      auto HasScheme(const Scheme& a,
                      const Scheme& b,
                      const Scheme& c,
-                     const Scheme& d) const;
-      bool HasScheme(const Scheme& a,
+                     const Scheme& d) const -> bool;
+      auto HasScheme(const Scheme& a,
                      const Scheme& b,
                      const Scheme& c,
                      const Scheme& d,
-                     const Scheme& e) const;
-      bool HasScheme(const Scheme& a,
+                     const Scheme& e) const -> bool;
+      auto HasScheme(const Scheme& a,
                      const Scheme& b,
                      const Scheme& c,
                      const Scheme& d,
                      const Scheme& e,
-                     const Scheme& f) const;
-      bool HasScheme(const std::unordered_set<Scheme>& values) const;
-      bool HasScheme(const std::set<Scheme>& values) const;
+                     const Scheme& f) const -> bool;
+      auto HasScheme(const std::unordered_set<Scheme>& values) const -> bool;
+      auto HasScheme(const std::set<Scheme>& values) const -> bool;
 
-      bool HasFragment() const {
+      auto HasFragment() const -> bool {
         return !fragment.empty();
       }
 
-      bool HasQuery() const {
+      auto HasQuery() const -> bool {
         return !query.empty();
       }
 
-      Extension GetExtension() const;
-      bool HasExtension() const;
-      bool HasExtension(const Extension& a) const;
-      bool HasExtension(const Extension& a,
-                        const Extension& b) const;
-      bool HasExtension(const Extension& a,
+      auto GetExtension() const -> Extension;
+      auto HasExtension() const -> bool;
+      auto HasExtension(const Extension& a) const -> bool;
+      auto HasExtension(const Extension& a,
+                        const Extension& b) const -> bool;
+      auto HasExtension(const Extension& a,
                         const Extension& b,
-                        const Extension& c) const;
-      bool HasExtension(const Extension& a,
+                        const Extension& c) const -> bool;
+      auto HasExtension(const Extension& a,
                         const Extension& b,
                         const Extension& c,
-                        const Extension& d) const;
-      bool HasExtension(const Extension& a,
+                        const Extension& d) const -> bool;
+      auto HasExtension(const Extension& a,
                         const Extension& b,
                         const Extension& c,
                         const Extension& d,
-                        const Extension& e) const;
-      bool HasExtension(const Extension& a,
+                        const Extension& e) const -> bool;
+      auto HasExtension(const Extension& a,
                         const Extension& b,
                         const Extension& c,
                         const Extension& d,
                         const Extension& e,
-                        const Extension& f) const;
-      bool HasExtension(const std::set<Extension>& extensions) const;
-      bool HasExtension(const std::unordered_set<Extension>& extensions) const;
+                        const Extension& f) const -> bool;
+      auto HasExtension(const std::set<Extension>& extensions) const -> bool;
+      auto HasExtension(const std::unordered_set<Extension>& extensions) const -> bool;
 
-      std::string GetResourceName() const {
+      auto GetResourceName() const -> std::string {
         const auto slashpos = path.find_last_of('/');
         if(slashpos == std::string::npos)
           return path;
         return path.substr(slashpos + 1);
       }
 
-      std::string GetPathWithoutExtension() const {
+      auto GetPathWithoutExtension() const -> std::string {
         const auto dotpos = path.find_first_of('.');
         if(dotpos != std::string::npos)
           return path.substr(0, path.size() - dotpos);
         return path;
       }
 
-      std::optional<std::string> GetQuery(const std::string& name) const {
+      auto GetQuery(const std::string& name) const -> std::optional<std::string> {
         const auto pos = query.find(name);
         if(pos == query.end())
           return std::nullopt;
         return { pos->second };
       }
 
-      Path GetParentPath() const {
+      auto GetParentPath() const -> Path {
         const auto slashpos = path.find_last_of('/');
         if(slashpos == std::string::npos)
           return "/";
         return path.substr(0, path.size() - (path.size() - slashpos));
       }
 
-      Uri GetParent() const {
-        return Uri(scheme, GetParentPath());
+      auto GetParent() const -> Uri {
+        return {scheme, GetParentPath()};
       }
 
-      Path GetChildPath(const Path& child) const {
+      auto GetChildPath(const Path& child) const -> Path {
         auto child_path = child;
         SanitizePath(child_path, false);
         return fmt::format("{0:s}/{1:s}", path, child_path);
       }
 
-      Uri GetChild(const Path& child) const {
-        return Uri(fmt::format("{0:s}://{1:s}", scheme, GetChildPath(child)));
+      auto GetChild(const Path& child) const -> Uri {
+        return {fmt::format("{0:s}://{1:s}", scheme, GetChildPath(child))};
       }
 
-      Path GetSiblingPath(const Path& sibling) const {
+      auto GetSiblingPath(const Path& sibling) const -> Path {
         auto sibling_path = sibling;
         SanitizePath(sibling_path, false);
         return fmt::format("{0:s}/{1:s}", GetParentPath(), sibling_path);
       }
 
-      Uri GetSibling(const Path& sibling) const {
-        return Uri(fmt::format("{0:s}://{1:s}", scheme, GetSiblingPath(sibling)));
+      auto GetSibling(const Path& sibling) const -> Uri {
+        return {fmt::format("{0:s}://{1:s}", scheme, GetSiblingPath(sibling))};
       }
 
-      Uri ToFileUri(const Path& root) const {
-        return Uri("file", root, *this);
+      auto ToFileUri(const Path& root) const -> Uri {
+        return {"file", root, *this};
       }
 
-      Uri ToFileUri() const {
-        return Uri("file", *this);
+      auto ToFileUri() const -> Uri {
+        return {"file", *this};
       }
 
-      bool IsFile() const {
+      auto IsFile() const -> bool {
         return EqualsIgnoreCase(scheme, "file");
       }
 
-      bool IsAbsolutePath() const {
+      auto IsAbsolutePath() const -> bool {
         return IsFile()
             && (FileExists(path));
       }
 
-      FILE* OpenFile(const char* mode) const {
+      auto OpenFile(const char* mode) const -> FILE* {
         PRT_ASSERT(HasScheme("file"));
         return fopen(path.c_str(), mode);
       }
 
-      inline FILE* OpenFileForReading() const {
+      inline auto OpenFileForReading() const -> FILE* {
         return OpenFile("rb");
       }
 
-      inline FILE* OpenFileForWriting() const {
+      inline auto OpenFileForWriting() const -> FILE* {
         return OpenFile("wb");
       }
 
-      std::string ToString() const;
+      auto ToString() const -> std::string;
 
       explicit operator std::string() const {
         return fmt::format("{0:s}://{1:s}", scheme, path);
       }
 
-      Uri& operator=(const Uri& rhs) = default;
+      auto operator=(Uri&& rhs) -> Uri& = default;
+      auto operator=(const Uri& rhs) -> Uri& = default;
 
-      bool operator==(const Uri& rhs) const {
+      auto operator==(const Uri& rhs) const -> bool {
         return EqualsIgnoreCase(scheme, rhs.scheme)
             && EqualsIgnoreCase(path, rhs.path);
       }
 
-      bool operator!=(const Uri& rhs) const {
+      auto operator!=(const Uri& rhs) const -> bool {
         return !EqualsIgnoreCase(scheme, rhs.scheme)
             || !EqualsIgnoreCase(path, rhs.path);
       }
 
-      friend std::ostream& operator<<(std::ostream& stream, const Uri& rhs) {
+      friend auto operator<<(std::ostream& stream, const Uri& rhs) -> std::ostream& {
         return stream << ((const std::string&) rhs);
       }
     };
 
-    static inline bool
-    IsDirectory(const Uri& uri) {
+    static inline auto
+    IsDirectory(const Uri& uri) -> bool {
       return prt::IsDirectory(uri.path);
     }
 
-    static inline bool
-    IsDirectory(const std::string& root, const Uri& uri) {
+    static inline auto
+    IsDirectory(const std::string& root, const Uri& uri) -> bool {
       const auto abs_path = fmt::format("{0:s}/{1:s}", root, uri.path);
       return prt::IsDirectory((const std::string&) abs_path);
     }
 
-    static inline bool
-    FileExists(const uri::Uri& uri) {
+    static inline auto
+    FileExists(const uri::Uri& uri) -> bool {
       return prt::FileExists(uri.path);
     }
 
-    bool TryParseUri(uri::Uri& result,
+    auto TryParseUri(uri::Uri& result,
                      const basic_uri uri,
-                     const uri::Scheme& default_scheme);
+                     const uri::Scheme& default_scheme) -> bool;
 
-    bool IsValidUri(const basic_uri& rhs);
-    bool IsValidUri(const basic_uri& rhs, const std::string& scheme);
-    bool IsValidUri(const basic_uri& rhs, const std::set<std::string>& schemes);
-    bool IsValidUri(const basic_uri& rhs, const std::unordered_set<std::string>& schemes);
+    auto IsValidUri(const basic_uri& rhs) -> bool;
+    auto IsValidUri(const basic_uri& rhs, const std::string& scheme) -> bool;
+    auto IsValidUri(const basic_uri& rhs, const std::set<std::string>& schemes) -> bool;
+    auto IsValidUri(const basic_uri& rhs, const std::unordered_set<std::string>& schemes) -> bool;
   }
 
-  static inline uint32_t
-  Murmur(const uri::Uri& uri) {
+  static inline auto
+  Murmur(const uri::Uri& uri) -> uint32_t {
     return Murmur((const std::string&) uri);
   }
 }
